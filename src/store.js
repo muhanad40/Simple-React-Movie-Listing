56765725b0418bd453779b7ab1017fe351aa89fa
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2018-10-02 06:49:57 +0100

    Enable dev tools

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
 export default createStore(
   rootReducer,
-  applyMiddleware(thunk)
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
